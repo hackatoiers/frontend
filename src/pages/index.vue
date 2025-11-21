9826ffcca8d8435d9fe48b7c6b342448a8569b62
rumians <ruanfort12@gmail.com>
2025-11-21 17:36:16 -0300

    refactor: comment out unused components in index.vue

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,13 @@
 <script setup>
 // import DetailItemComp from '@/components/detailItem/DetailItemComp.vue';
-import HeaderComp from '@/components/header/HeaderComp.vue';
-import HomeView from '@/components/home/HomeView.vue';
+// import HeaderComp from '@/components/header/HeaderComp.vue';
+// import HomeView from '@/components/home/HomeView.vue';
 </script>
 <template>
-    <HeaderComp />
-    <HomeView />
+    HOME
+    <!-- <HeaderComp />
+    <HomeView /> -->
     <!-- <DetailItemComp /> -->
-    <div>
-
-    </div>
 </template>
 <style scoped>
 
