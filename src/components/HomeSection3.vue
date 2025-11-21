039a92e2427bd44f73d57dc66ec6f8c013424088
marcoperuzzocmd <marcoantperuzzo@gmail.com>
2025-11-21 18:59:39 -0300

    feat:  implementando section-3 no sistema

---
+++ src/components/HomeSection3.vue
@@ -0,0 +1,86 @@
+<script setup></script>
+
+<template>
+  <div class="content">
+    <img src="/public/img-section3.svg" alt="">
+    <div class="info">
+      <h1 style="margin-left: 40px; margin-top: 30px">Informações</h1>
+      <div class="infos">
+        <img src="/public/relogio.svg" alt="">
+        <div class="text-info">
+          <h3>Horario</h3>
+          <p>Terça a Domingo: 9h às 17h</p>
+          <p>Fechado às segundas-feiras</p>
+        </div>
+      </div>
+      <div class="infos"><img src="/public/location.svg" alt="">
+        <div class="text-info">
+          <h3>Localização</h3>
+          <p>Rua Dona Francisca, 600</p>
+          <p>Centro, Joinville - SC</p>
+        </div>
+      </div>
+      <div class="infos"><img src="/public/phone.svg" alt="">
+        <div class="text-info">
+          <h3>Telefone</h3>
+          <p>(47) 3433-3000</p>
+        </div>
+      </div>
+      <div class="infos"><img src="/public/mail.svg" alt="">
+        <div class="text-info">
+          <h3>Contato</h3>
+          <p>contato@museusambaqui.org.br</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.content {
+  width: 1344px;
+  height: 445px;
+  margin: auto;
+  display: flex;
+  gap: 75px;
+}
+
+.image {
+  width: 60%;
+  height: 100%;
+  border-radius: 10px;
+}
+
+.info {
+  width: 40%;
+  height: 100%;
+  border-radius: 10px;
+  border: 1px solid #E2E8F0;
+  box-shadow: 0 4px 12px rgba(0,0,0,0.08); /* sombra leve */
+
+}
+
+img {
+  width: 100%;
+  height: 100%;
+}
+
+.infos {
+  display: flex;
+  gap: 35px;
+  margin-top: 30px;
+  align-items: center;
+  margin-left: 45px;
+}
+.infos>img {
+  width: 40px;
+  height: 40px;
+}
+.text-info > h3{
+  font-size: 15px;
+  font-weight: 900;
+}
+.text-info > p{
+  font-size: 15px;
+}
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,22 +1,26 @@
 <script setup>
+import HomeSection3 from '@/components/HomeSection3.vue';
+
+
 // import DetailItemComp from '@/components/detailItem/DetailItemComp.vue';
 // import HeaderComp from '@/components/header/HeaderComp.vue';
 
-import { useAuth } from '@/stores/auth';
+/*import { useAuth } from '@/stores/auth';
 import { RouterLink } from 'vue-router';
-const auth = useAuth();
+const auth = useAuth();*/
 </script>
 <template>
-    HOME    
+    HOME
+    <HomeSection3/>
     <!-- <DetailItemComp /> -->
     <!-- <HeaderComp /> -->
-    <p v-if="auth.state.user">Logado como {{ auth.state.user?.email }} <button @click="auth.logout()">Sair</button></p>
-    <RouterLink to="/auth/login" v-else>Login</RouterLink>
+    <!--<p v-if="auth.state.user">Logado como {{ auth.state.user?.email }} <button @click="auth.logout()">Sair</button></p>
+    <RouterLink to="/auth/login" v-else>Login</RouterLink>-->
 </template>
 <style scoped>
-button, a {
+/*button, a {
     cursor: pointer;
     background-color: red;
-}
+}*/
 
 </style>
