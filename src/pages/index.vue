f607628f02dad97dd2d1c4e04482e4f5aaa791cf
ohdavir <davi.rosa.info@gmail.com>
2025-11-22 02:25:52 -0300

    feat: add scroll and interactive effects to HomeView component; update HeaderComp and HeaderButton styles

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,14 +1,37 @@
 <script setup>
-import DetailItemComp from '@/components/detailItem/DetailItemComp.vue';
-import HeaderComp from '@/components/header/HeaderComp.vue';
-import HomeView from '@/components/home/HomeView.vue';
+// import DetailItemAdmin from '@/components/detailItem/DetailItemAdmin.vue';
+//import HomeSection3 from '@/components/HomeSection3.vue';
+// import DetailItemComp from '@/components/detailItem/DetailItemComp.vue';
+// import HeaderComp from '@/components/header/HeaderComp.vue';
+// import Footer from '@/components/Footer.vue';
+// import Card from '@/components/card.vue';
+// import { useAuth } from '@/stores/auth';
+// import { RouterLink } from 'vue-router';
+// const auth = useAuth();
+// import ClassesComp from '@/components/classes/ClassesComp.vue';
+// import CadastroPComp from '@/components/cadastrar-produto/CadastroPComp.vue';
+// import SectionCards from '@/components/sectionCards.vue';
+// import ModalReservaComp from '@/components/modal-reserva/ModalReservaComp.vue';
 </script>
 <template>
-    
-    <HeaderComp />
-    <HomeView />
+HOME
+<!-- <SectionCards /> -->
+<!--<DetailItemAdmin/> -->
+    <!--<CadastroPComp /> -->
+    <!-- <CadastroPComp /> -->
+    <!-- <CadastroPComp /> -->
+    <!-- <ModalReservaComp :open="true" /> -->
+    <!-- <Card /> -->
+    <!-- <ClassesComp /> -->
     <!-- <DetailItemComp /> -->
+    <!-- <HeaderComp /> -->
+     <!-- <Footer /> -->
+    <!-- <p v-if="auth.state.user">Logado como {{ auth.state.user?.email }} <button @click="auth.logout()">Sair</button></p>
+    <RouterLink to="/auth/login" v-else>Login</RouterLink> -->
 </template>
 <style scoped>
-
+/* button, a {
+    cursor: pointer;
+    background-color: red;
+} */
 </style>
